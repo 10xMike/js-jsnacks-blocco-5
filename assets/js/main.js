5cbe868d7808eb38ad4d3d32c7dba727898c8111
10xMike <michelegec@gmail.com>
2021-05-11 11:17:16 +0200

    Snack 5

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,3 +49,24 @@ console.log(uniteArray(arrayString, arrayNum));
 // 5. Scrivi una funzione che accetti tre argomenti:
 // un array e due numeri (“a” più piccolo di “b” e “b” grande al massimo quanto il numero di elementi dell’array).
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra “a” e “b”
+
+function filterArray(lista, min, max) {
+    /* console.log(lista, min, max); */
+    var nuovoArray = [];
+    for (var i = 0; i < lista.length; i++) {
+        var El = lista[i];
+        if(min <= i && max >= i){
+            nuovoArray.push(lista[i]);
+        }
+    }
+    console.log(lista, min, max);
+    return nuovoArray;
+}
+
+var nomi = ['filippo', 'marco', 'giorgio', 'valerio', 'michele', 'luigi'];
+var min = 1;
+var max = 3;
+filterArray(nomi, min, max);
+console.log(filterArray(nomi, min, max));
+var risultato = filterArray(nomi, min, max);
+console.log(risultato);
